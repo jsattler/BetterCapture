dd1106ba8399d83d7630e319b4665588e5dbd1b9
Joshua Sattler <34030048+jsattler@users.noreply.github.com>
2026-01-29 19:39:01 +0100

    Initial Commit

---
+++ BetterCapture/BetterCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterCaptureApp.swift
+//  BetterCapture
+//
+//  Created by Joshua Sattler on 29.01.26.
+//
+
+import SwiftUI
+
+@main
+struct BetterCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterCaptureTests/BetterCaptureTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetterCaptureTests.swift
+//  BetterCaptureTests
+//
+//  Created by Joshua Sattler on 29.01.26.
+//
+
+import Testing
+@testable import BetterCapture
+
+struct BetterCaptureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
